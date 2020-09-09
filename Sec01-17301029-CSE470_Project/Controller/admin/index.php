4d791af222eb5d0916bf7297501ee7879f284ff5
Maria-Khan00 <71029439+Maria-Khan00@users.noreply.github.com>
2020-09-09 23:29:03 +0600

    Add files via upload

---
+++ Sec01-17301029-CSE470_Project/Controller/admin/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+	<title>Admin Home</title>
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+	<script type="text/javascript">
+		function sureToApprove(id){
+			if(confirm("Are you sure you want to delete this message?")){
+				window.location.href ='delete_msg.php?id='+id;
+			}
+		}
+	</script>
+</head>
+<body>
+<!-- Header -->
+<div id="header">
+	<div class="shell">
+		
+		<?php
+			include 'menu.php';
+		?>
+		</div>
+		<!-- End Main Nav -->
+	</div>
+</div>
+
+<div id="container">
+	<div class="shell">
+		
+		
+		
+		<br />
+		
+		<div id="main">
+			<div class="cl">&nbsp;</div>
+			
+			<div id="content">
+				
+				<h1 style="color:#000">welcome to admin panel</h1>
+ 
+			</div>
+			
+			
+			<div class="cl">&nbsp;</div>			
+		</div>
+		<!-- Main -->
+	</div>
+</div>
+	
+</body>
+</html>
---
+++ Sec01-17301029-CSE470_Project/View/wait.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>PET STORE</title>
+	<meta charset="utf-8">
+	<meta name="author" content="pixelhint.com">
+	<meta name="description" content="La casa free real state fully responsive html5/css3 home page website template"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0" />
+	
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/responsive.css">
+
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/main.js"></script>
+</head>
+<body>
+
+	<section class="">
+		<?php
+			include 'header.php';
+		?>
+
+			<section class="caption">
+				<h2 class="caption" style="text-align: center">Find You Dream pets</h2>
+				
+			</section>
+	</section><!--  end hero section  -->
+
+
+
+
+
+	<section class="listings">
+		<div class="wrapper">
+			<ul class="properties_list">
+				<h2 style="text-align:center; color:#FF0000; font-family: 'Courier New', Courier, monospace">Thank you for sending a request to us. 
+				We will get back to you once we verify your payment.
+				</h2>
+			</ul>
+			
+		</div>
+	</section>
+	
+</body>
+</html>
